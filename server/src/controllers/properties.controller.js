1a09e83c68dfb6fafd5e406c334a95db16352a41
Chris Chen <chrischen79@gmail.com>
2020-05-15 12:19:20 +1000

    server: move mock data keyword searching code to the right place

--- server/src/controllers/properties.controller.js
+++ server/src/controllers/properties.controller.js
@@ -4,27 +4,11 @@
  * at the moment, it only exports one async search function
  */
 
-const idx = require('idx');
 const { PropertiesDataProvider } = require('../models');
 
 // search function is agnostic to the underlying data storage mechanism
 const searchBySuburb = async (suburb) => {
-  const properties = await PropertiesDataProvider.propertiesBySuburb(suburb);
-
-  // filter properties by suburb keyword
-  // assumption: property address string is formatted as "2403 Forster Circle, Carlton"
-  let result = properties.filter((property) => {
-    const propertySuburb = idx(property, (property) => property.address.split(', ')[1]);
-
-    if (propertySuburb) {
-      return propertySuburb.toLowerCase().includes(suburb.toLowerCase());
-    }
-
-    // filter out property without a valid address property
-    return false;
-  });
-
-  return result;
+  return await PropertiesDataProvider.propertiesBySuburb(suburb);
 };
 
 module.exports = { searchBySuburb };
--- server/src/models/properties/mock.provider.js
+++ server/src/models/properties/mock.provider.js
@@ -1,7 +1,21 @@
+const idx = require('idx');
 const mockProperties = require('./mock.json');
 
 const propertiesBySuburb = async (suburb) => {
-  return mockProperties;
+  // filter properties by suburb keyword
+  // assumption: property address string is formatted as "2403 Forster Circle, Carlton"
+  let result = mockProperties.filter((property) => {
+    const propertySuburb = idx(property, (property) => property.address.split(', ')[1]);
+
+    if (propertySuburb) {
+      return propertySuburb.toLowerCase().includes(suburb.toLowerCase());
+    }
+
+    // filter out property without a valid address property
+    return false;
+  });
+
+  return result;
 };
 
 module.exports = { propertiesBySuburb };
